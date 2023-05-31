e7c0b10b297684fa6bca342695f3b1425e7b5439
Zeynel Abiddin Aydar <70782441+ZEABAY@users.noreply.github.com>
2023-05-31 03:40:10 +0300

    Update Main.java
    fix

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Basamak Sayısı : ");
-        int n = scan.nextInt();
+        //Kaç katlı üçgen çizilecek
+        System.out.print("Kat : ");
+        int level = scan.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-
+        for (int i = 1; i <= level; i++) {
+            //Boşlukları yaz
             for (int j = 1; j < i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= 2 * (n - i) + 1; j++) {
+            //Yıldızları yaz
+            for (int j = 1; j <= 2 * (level - i) + 1; j++) {
                 System.out.print("*");
             }
 
